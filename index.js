03ee354d6254272cd76c4db62a15445546fd831a
steveafrost <ffrostt@gmail.com>
2017-03-31 22:57:05 -0400

    practiced using createElement and render

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-//Code React element here
+const headline = React.createElement('h1', {}, 'An Awesome Person');
+const paragraph = React.createElement('p', {}, 'Who is learning React');
+const list = React.createElement('ul', {className: 'me__interests'},
+  React.createElement('li', {}, 'JavaScript'),
+  React.createElement('li', {}, 'React'),
+  React.createElement('li', {}, 'Movies'),
+  React.createElement('li', {}, 'Ice cream')
+);
+const meInReact = React.createElement('div', {className: 'me'}, [headline, paragraph, list]);
+
+ReactDOM.render(
+  meInReact,
+  document.getElementById('main')
+);
